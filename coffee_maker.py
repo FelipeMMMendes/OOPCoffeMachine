226fdcef02f1fa303dfa11c0d5273d8de49537b0
Felipe Martins <felipe_mmmachado@hotmail.com>
2022-09-12 16:14:08 -0300

    version 1.0

--- coffee_maker.py
+++ coffee_maker.py
@@ -2,9 +2,9 @@ class CoffeeMaker:
     """Models the machine that makes the coffee"""
     def __init__(self):
         self.resources = {
-            "water": 300,
-            "milk": 200,
-            "coffee": 100,
+            "water": 2000,
+            "milk": 500,
+            "coffee": 500,
         }
 
     def report(self):
--- main.py
+++ main.py
@@ -1,3 +1,48 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+#instancia os objetos
+coffeMaker = CoffeeMaker()
+moneyMachine = MoneyMachine()
+menu = Menu()
+
+
+#variavel flag para controlar a repeticao da maquina
+machineOn = True
+
+while machineOn == True:
+
+    #pergunta para o usuario qual bebida ele vai querer
+    drink = input((f"What would you like? {menu.get_items()}\n"))
+    
+    #se o usuario entrar com off, desliga a maquina
+    if drink == 'off':
+        machineOn = False
+
+    #se o usuario entrar com report, mostra os status da maquina
+    elif drink == 'report':
+        coffeMaker.report()
+        moneyMachine.report()
+
+    else:    
+        #associa a bebida ao que o usuario digitou
+        drink = menu.find_drink(drink)    
+
+        #checa se há recursos o suficiente para fazer a bebida
+        if coffeMaker.is_resource_sufficient(drink) == False:
+            print("Not enough ingredients available, why don't you try another drink? ")
+            continue
+        else:
+            #chama uma funcao para contar a quantidade de dinheiro inserido
+            if moneyMachine.make_payment(drink.cost):
+                #faz a bebida que foi pedida
+                coffeMaker.make_coffee(drink)
+
+
+
+        
+
+
+
+   
